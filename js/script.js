eada97444ca2d822d665084ddb4c60f3ed8ac5dc
silestefan <147498570+silestefan@users.noreply.github.com>
2024-01-21 16:04:38 +0000

    Added funtion for actual time display

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 // Display of the current date and time
+    function displayTime() {
+    var currentTime = dayjs().format('dddd, MMMM D, YYYY, h:mm A');
+    $('#currentDay').text(currentTime);
+    }
 // Create the time blocks with the columns for working hours
 // Add the save button for each column 
 // Add the text area for user to input
 // Create the classes based on past present and future
 // Save the uset input in the local storage
+displayTime();
